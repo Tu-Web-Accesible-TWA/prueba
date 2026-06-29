3f4ea7350f6b7c38f19aedd6dbeae161dae6d23b
dlg383 <dlg383@inlumine.ual.es>
2026-06-29 14:17:48 +0200

    line

--- prueba/src/app/app.ts
+++ prueba/src/app/app.ts
@@ -1,36 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { List } from './list/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, List],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('prueba');
-  
-  data = [
-    {
-      id: 1,
-      name: 'prueba'
-    },
-    {
-      id: 2,
-      name: 'prueba'
-    },
-    {
-      id: 3,
-      name: 'prueba'
-    },
-    {
-      id: 4,
-      name: 'prueba'
-    },
-    {
-      id: 5,
-      name: 'prueba'
-    }
-  ]
 }
